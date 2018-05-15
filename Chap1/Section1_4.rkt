338f1bccad4e64eb3f773d5750d6a3188358f5bb
Saltb0rn <asche34@outlook.com>
2018-05-15 11:32:32 +0800

    Finished 1.19, 1.20 and 1.21

--- Chap1/Section1_4.rkt
+++ Chap1/Section1_4.rkt
@@ -85,3 +85,67 @@
 		s1 s2 the-rest
 		(lambda (val2)
 		  (cont (cons val1 val2))))))))))
+
+;; 1.19
+;; list-set : list? * number? * any -> list?
+(define (list-set lst n x)
+  (list-set/k lst n x (lambda (val) val)))
+
+;; list-set/k list? * number? * any * cont -> list?
+(define (list-set/k lst n x cont)
+  (if (null? lst)
+      (cont '())
+      (list-set/k (cdr lst) (sub1 n) x
+		  (lambda (val)
+		    (cont
+		     (cons
+		      (if (zero? n) x (car lst))
+		      val))))))
+
+;; 1.20
+;; count-occurrences : symbol? * (listof symbol?) -> number?
+(define (count-occurences s slist)
+  (count-occurences/k s slist (lambda (val) val)))
+
+;; count-occurences/k : symbol? * (listof symbol?) * cont -> number?
+(define (count-occurences/k s slist cont)
+  (if (null? slist)
+      (cont 0)
+      (count-occurences/k s (cdr slist)
+			  (lambda (val1)
+			    (let ((sexp (car slist)))
+			      (if (symbol? sexp)
+				  (if (eqv? s sexp)
+				      (cont (add1 val1))
+				      (cont val1))
+				  (count-occurences/k
+				   s sexp
+				   (lambda (val2)
+				     (cont (+ val1 val2))))))))))
+
+;; 1.21
+;; product : list? * list? -> (listof pair?)
+(define (product sos1 sos2)
+  (product/k sos1 sos2 (lambda (val) val)))
+
+;; product : list? * list? * cont -> (listof pair?)
+(define (product/k sos1 sos2 cont)
+  (if (or (null? sos1) (null? sos2))
+      (cont '())
+      (product/k
+       (cdr sos1) sos2
+       (lambda (val1)
+	 (combine-one/k
+	  (car sos1) sos2
+	  (lambda (val2)
+	    (cont (cons val2 val1))))))))
+
+;; combine-one : symbol? * list? * cont -> (listof pair?)
+(define (combine-one/k s sos2 cont)
+  (if (null? sos2)
+      (cont '())
+      (combine-one/k
+       s (cdr sos2)
+       (lambda (val1)
+	 (cont
+	  (cons (cons s (list (car sos2))) val1))))))
