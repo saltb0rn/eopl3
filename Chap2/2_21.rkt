c98ea810ca0964e8541be5ff850bd1c5ed28c855
Saltb0rn <asche34@outlook.com>
2018-07-05 10:00:32 +0800

    Add 2.21

---
+++ Chap2/2_21.rkt
@@ -0,0 +1,19 @@
+#lang eopl
+
+(define-datatype environment environment?
+  (empty-env)
+  (extend-env (identifier symbol?)
+	      (value number?)
+	      (saved-env environment?)))
+
+(define (apply-env env searched-var)
+  (cases environment env
+	 (empty-env () (eopl:error 'Environment "~a is not bound" searched-var))
+	 (extend-env (identifier value saved-env)
+		     (if (eqv? identifier searched-var)
+			 value
+			 (apply-env saved-env searched-var)))))
+
+
+
+
