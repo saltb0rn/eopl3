3b5e3607f9542375bdbc407a98121a78ceeb6fed
saltb0rn <asche34@outlook.com>
2018-11-14 00:45:55 +0800

    Add letrec-lang-register

--- langs/letrec-lang-register/interpreter.rkt
+++ langs/letrec-lang-register/interpreter.rkt
@@ -3,6 +3,13 @@
 (require eopl
          "lang.rkt")
 
+;;;;;;;;;;;;;;;;;; Register ;;;;;;;;;;;;;;;;;;
+(define reg-exp 'uninitialized)
+(define reg-env 'uninitialized)
+(define reg-cont 'uninitialized)
+(define reg-val 'uninitialized)
+(define reg-proc 'uninitialized)
+
 ;;;;;;;;;;;;;;;;;; Values ;;;;;;;;;;;;;;;;;;
 (define-datatype expval expval?
   [num-val (value number?)]
@@ -14,13 +21,15 @@
 (define procedure
   (lambda (var body saved-env)
     (lambda (val cont)
-      (value-of/k body (extend-env var val saved-env) cont))))
+      (set! reg-cont cont)
+      (set! reg-env (extend-env var val saved-env))
+      (set! reg-exp body)
+      (value-of/k))))
 
 ;; apply-procedure : Proc * ExpVal * Cont -> FinalAnswer
 (define apply-procedure/k
-  (lambda (proc1 val1 cont)
-    (lambda ()
-      (proc1 val1 cont))))
+  (lambda ()
+    (reg-proc reg-val reg-cont)))
 
 (define expval->num
   (lambda (v)
@@ -77,38 +86,47 @@
 
 ;; apply-cont : Cont * ExpVal -> FinalAnswer
 (define apply-cont
-  (lambda (cont val)
-    (cases continuation cont
+  (lambda ()
+    (cases continuation reg-cont
            [end-cont ()
                      (begin
                        (eopl:printf "End of computation.~%")
-                       val)]
+                       reg-val)]
            [zero1-cont (saved-cont)
-                       (apply-cont saved-cont
-                                   (bool-val
-                                    (zero? (expval->num val))))]
+                       (set! reg-cont saved-cont)
+                       (set! reg-val (bool-val (zero? (expval->num reg-val))))
+                       (apply-cont)]
            [let-exp-cont (var body saved-env saved-cont)
-                         (value-of/k
-                          body
-                          (extend-env var val saved-env)
-                          saved-cont)]
+                         (set! reg-env (extend-env var reg-val saved-env))
+                         (set! reg-cont saved-cont)
+                         (set! reg-exp body)
+                         (value-of/k)]
            [if-test-cont (exp1 exp2 saved-env saved-cont)
-                         (if (expval->bool val)
-                             (value-of/k exp1 saved-env saved-cont)
-                             (value-of/k exp2 saved-env saved-cont))]
+                           (set! reg-exp (if (expval->bool reg-val) exp1 exp2))
+                           (set! reg-env saved-env)
+                           (set! reg-cont saved-cont)
+                           (value-of/k)]
            [diff1-cont (exp2 saved-env saved-cont)
-                       (value-of/k exp2 saved-env (diff2-cont val saved-cont))]
+                       (set! reg-env saved-env)
+                       (set! reg-cont (diff2-cont reg-val saved-cont))
+                       (set! reg-exp exp2)
+                       (value-of/k)]
            [diff2-cont (val1 saved-cont)
                        (let ([num1 (expval->num val1)]
-                             [num2 (expval->num val)])
-                         (apply-cont saved-cont (num-val (- num1 num2))))]
+                             [num2 (expval->num reg-val)])
+                         (set! reg-val (num-val (- num1 num2)))
+                         (set! reg-cont saved-cont)
+                         (apply-cont))]
            [rator-cont (rand saved-env saved-cont)
-                       (value-of/k rand saved-env
-                                   (rand-cont val saved-cont))]
+                       (set! reg-env saved-env)
+                       (set! reg-cont (rand-cont reg-val saved-cont))
+                       (set! reg-exp rand)
+                       (value-of/k)]
            [rand-cont (val1 saved-cont)
-                      (apply-procedure/k
-                       (expval->proc val1)
-                       val saved-cont)]
+                      (set! reg-proc (expval->proc val1))
+                      (set! reg-cont saved-cont)
+                      (set! reg-val reg-val)
+                      (apply-procedure/k)]
            )))
 
 
@@ -160,51 +178,50 @@
   (lambda (pgm)
     (cases program pgm
            (a-program (exp1)
-                      (trampoline (value-of/k exp1 (init-env) (end-cont)))))))
+                      (set! reg-exp exp1)
+                      (set! reg-env (init-env))
+                      (set! reg-cont (end-cont))
+                      (value-of/k)))))
 
 ;; value-of : Exp * Env * Cont -> ExpVal
 (define value-of/k
-  (lambda (exp env cont)
-    (cases expression exp
-           [const-exp (num) (apply-cont cont (num-val num))]
-           [var-exp (var) (apply-cont cont (apply-env env var))]
+  (lambda ()
+    (cases expression reg-exp
+           [const-exp (num)
+                      (set! reg-val (num-val num))
+                      (apply-cont)]
+           [var-exp (var)
+                    (set! reg-val (apply-env reg-env var))
+                    (apply-cont)]
            [diff-exp (exp1 exp2)
-                     (value-of/k exp1 env
-                                 (diff1-cont exp2 env cont))]
+                     (set! reg-cont (diff1-cont exp2 reg-env reg-cont))
+                     (set! reg-exp exp1)
+                     (value-of/k)]
            [zero?-exp (exp1)
-                      (value-of/k exp1 env (zero1-cont cont))]
+                      (set! reg-exp exp1)
+                      (set! reg-cont (zero1-cont reg-cont))
+                      (value-of/k)]
            [if-exp (exp1 exp2 exp3)
-                   (value-of/k
-                    exp1
-                    env
-                    (if-test-cont exp2 exp3 env cont))]
+                   (set! reg-exp exp1)
+                   (set! reg-cont (if-test-cont exp2 exp3 reg-env reg-cont))
+                   (value-of/k)]
            [let-exp (var exp1 body)
-                    (value-of/k
-                     exp1
-                     env
-                     (let-exp-cont var body env cont))]
+                    (set! reg-exp exp1)
+                    (set! reg-cont (let-exp-cont var body reg-env reg-cont))
+                    (value-of/k)]
            [proc-exp (var body)
-                     (apply-cont
-                      cont
-                      (proc-val (procedure var body env)))]
+                     (set! reg-val (proc-val (procedure var body reg-env)))
+                     (apply-cont)]
            [call-exp (rator rand)
-                     (value-of/k
-                      rator
-                      env
-                      (rator-cont rand env cont))]
+                     (set! reg-exp rator)
+                     (set! reg-cont (rator-cont rand reg-env reg-cont))
+                     (value-of/k)]
            [letrec-exp (proc-name var proc-body letrec-body)
-                       (let ((new-env (extend-env-rec proc-name var proc-body env)))
-                         (value-of/k letrec-body new-env cont))]
+                       (let ((new-env (extend-env-rec proc-name var proc-body reg-env)))
+                         (set! reg-env new-env)
+                         (set! reg-exp letrec-body)
+                         (value-of/k))]
            )))
-
-
-;; trampoline : Bounce -> FinalAnswer
-(define trampoline
-  (lambda (bounce)
-    (if (expval? bounce)
-        bounce
-        (trampoline (bounce)))))
-
 
 ;;;;;;;;;;;;;;;;;; Example ;;;;;;;;;;;;;;;;;;
 
@@ -215,5 +232,6 @@
              [num-val (v) v]
              [bool-val (v) v]
              [proc-val (v) v]))))
+
 
 (run "letrec p (z) = if zero?(z) then 1 else (p -((p -(z,1)),1)) in (p 11)")
